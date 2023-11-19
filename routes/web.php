405a219c41cf62b78eedc3fe48f8362ea76504a7
AlifsukaMERAH <nasywa.muhammad70@gmail.com>
2023-11-19 14:30:10 +0700

    second commit

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,8 @@ use App\Http\Controllers\ContactController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\WishlistController;
 use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\adminIndexController;
+use App\Http\Controllers\adminTablesController;
 
 Auth::routes();
 Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
@@ -22,3 +24,7 @@ Route::get('/confirmation', [CartController::class, 'confirmation'])->name('conf
 Route::get('/wishlist', [WishlistController::class, 'wishlist'])->name('wishlist');
 Route::get('/contact', [ContactController::class, 'contact'])->name('contact');
 
+//route for admin
+Route::get('/indexadmin',[adminIndexController::class,'index'])->name('indexadmin');
+Route::get('/indextables',[adminTablesController::class,'index'])->name('tableadmin');
+Route::resource('user',adminTablesController::class);
